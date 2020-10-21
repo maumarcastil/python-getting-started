83657923e018222e449e454f9301ab2cd82bab32
unknown <cadicossamm@gmail.com>
2020-10-21 17:28:10 -0500

    first commit

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -6,16 +6,11 @@ admin.autodiscover()
 
 import hello.views
 
-# To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
-
 urlpatterns = [
-    path("", hello.views.index, name="index"),
+    path("", hello.views.index, name="Index"),
     path("helloworld/", hello.views.helloworld, name="helloworld"),
+    path("create_article/<str:title>/<str:content>/", hello.views.create_article, name="create_article"),
+    path("get_article/", hello.views.get_article, name="get_article"),
+    path("update_article/<str:id>/", hello.views.update_article, name="update_article"),
     path("admin/", admin.site.urls),
 ]
